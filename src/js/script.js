2e2c467efc9aa8b9da0323edd1cfe2e40e3a5812
Verbrugghejara <jara.verbrugghe@student.howest.be>
2025-10-03 09:42:16 +0200

    flying

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,7 @@ import * as THREE from 'three';
 
 const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(90, window.innerWidth/window.innerHeight, 0.1, 1000);
 camera.position.set(0, 2, 5);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: canvas });
