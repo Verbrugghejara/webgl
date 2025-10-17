9f6d8bd93765479468cbf264217103ff265d2a6a
Verbrugghejara <jara.verbrugghe@student.howest.be>
2025-10-17 10:34:11 +0200

    change imports

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'https://unpkg.com/three@0.164.1/build/three.module.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.164.1/examples/jsm/loaders/GLTFLoader.js';
 
 import landscapeVertexShader from './shaders/vertex.glsl';
 import landscapeFragmentShader from './shaders/fragment.glsl';
