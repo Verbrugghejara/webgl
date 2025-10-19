358217adb98cb8ff786e1507b38f6147142c015e
Verbrugghejara <jara.verbrugghe@student.howest.be>
2025-10-19 17:04:22 +0200

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,5 @@
 import { defineConfig } from 'vite'
-import glsl from 'vite-plugin-glsl'
 
 export default defineConfig({
-  plugins: [glsl()],
   base: '/webgl/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        assetFileNames: 'assets/[name].[ext]',
-        chunkFileNames: 'assets/[name].js',
-        entryFileNames: 'assets/[name].js'
-      }
-    }
-  }
-})
+});
